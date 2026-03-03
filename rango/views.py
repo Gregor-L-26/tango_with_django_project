ef7fe59496d405b999168b11bbf6a4ddd987e34d
Gregor Loftus <3174297L@student.gla.ac.uk>
2026-03-03 14:41:11 +0000

    create the add category view

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from rango.forms import CategoryForm
 from rango.models import Category
 from rango.models import Page
+from django.shortcuts import redirect
+
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -14,9 +18,11 @@ def index(request):
 
     return render(request, 'rango/index.html', context=context_dict)
 
+
 def about(request):
     context_dict = {'boldmessage': 'This tutorial has been put together by Gregor'}
     return render(request, 'rango/about.html', context=context_dict)
+
 
 def show_category(request, category_name_slug):
     context_dict = {}
@@ -31,3 +37,18 @@ def show_category(request, category_name_slug):
         context_dict['pages'] = None
 
     return render(request, 'rango/category.html', context=context_dict)
+
+
+def add_category(request):
+    form = CategoryForm()
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return redirect('/rango/')
+        else:
+            print(form.errors)
+
+    return render(request, 'rango/add_category.html', {'form': form})
