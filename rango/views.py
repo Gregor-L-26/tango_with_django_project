94133ac4fdf244f02f1376b7e8e4c259b52a5d16
Gregor Loftus <3174297L@student.gla.ac.uk>
2026-03-03 19:36:44 +0000

    Restricted page made for only logged in users

--- rango/views.py
+++ rango/views.py
@@ -9,6 +9,7 @@ from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 from django.urls import reverse
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
@@ -139,3 +140,7 @@ def user_login(request):
 
     else:
         return render(request, 'rango/login.html')
+
+@login_required
+def restricted(request):
+    return HttpResponse("Since you're logged in, you can see this text!")
