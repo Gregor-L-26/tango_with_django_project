c9f6f4f9e1230480ffa4ca02c96b28774847ca6f
Gregor Loftus <3174297L@student.gla.ac.uk>
2026-03-03 21:00:35 +0000

    Made use of new helper method to fix test from failing

--- rango/views.py
+++ rango/views.py
@@ -162,9 +162,9 @@ def user_logout(request):
     return redirect(reverse('rango:index'))
 
 def visitor_cookie_handler(request):
-    visits = int(request.COOKIES.get('visits', '1'))
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
 
-    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
     last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
 
     if (datetime.now() - last_visit_time).days > 0:
