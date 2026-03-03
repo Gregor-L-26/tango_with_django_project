5c32e3c16480379c2dceb4d1e194fd8460d5d332
Gregor Loftus <3174297L@student.gla.ac.uk>
2026-03-03 20:39:27 +0000

    Removed visters from index view

--- rango/views.py
+++ rango/views.py
@@ -18,13 +18,11 @@ def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
 
+    visitor_cookie_handler(request)
+
     context_dict = {}
-    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
-
-    visitor_cookie_handler(request)
-    context_dict['visits'] = request.session('visits')
 
     response = render(request, 'rango/index.html', context=context_dict)
     return response
