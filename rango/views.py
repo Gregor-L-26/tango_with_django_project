13551293f60d6ac06f0a9e3f8e7e1b617de372ee
Gregor Loftus <3174297L@student.gla.ac.uk>
2026-03-03 19:47:42 +0000

    Made restricted page follow the base template

--- rango/views.py
+++ rango/views.py
@@ -145,7 +145,7 @@ def user_login(request):
 
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    return request(request, 'rango/restricted.html')
 
 @login_required
 def user_logout(request):
